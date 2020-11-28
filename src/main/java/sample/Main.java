c4e45f0aa45941b5ea507ddd48fe747a6f8af77c
Léo Variéras <70764329+Lvarieras@users.noreply.github.com>
2020-11-28 19:58:19 +0100

    Interface : selection des différents modules et prélancement des extracteurs

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -11,31 +11,50 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        final ComboBox<String> comboBox = new ComboBox();
-        comboBox.getItems().setAll("Exctracteur 1","Extracteur 2", "Extracteur 3");
-        comboBox.valueProperty().addListener(observable -> System.out.printf("Valeur sélectionnée: %s", comboBox.getValue()).println());
-        comboBox.getSelectionModel().select(1);
-        primaryStage.setTitle("Exctrator");
-        Button btn = new Button();
-        btn.setText("Lancez votre extracteur");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+    String comboBoxValue = "";
+    int index = 0;
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("L'extracteur n°");
-            }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        final ComboBox<String> comboBox = new ComboBox();
+        comboBox.getItems().setAll("Exctracteur 1", "Extracteur 2", "Extracteur 3");
+        Button btn = new Button();
+        comboBox.valueProperty().addListener(observable -> {
+
+            comboBoxValue = comboBox.getSelectionModel().getSelectedItem().toString();
+            index = comboBox.getSelectionModel().getSelectedIndex();
+
+
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+
+                @Override
+                public void handle(ActionEvent event) {
+                    if (index == 0) {
+                        System.out.println("//lancer l'ancien Java l'extracteur Java witext ");
+                    } else if (index == 1) {
+                        System.out.println("//Lancer le Java sur le CSV");
+                    } else {
+                        System.out.println("//Lancer le python");
+                    }
+                    System.out.println("L'extracteur selectionné est l'" + comboBoxValue);
+                }
+            });
         });
+        btn.setTranslateX(75);
+        btn.setTranslateY(100);
+        primaryStage.setTitle("Exctrator");
+        btn.setText("Lancez votre extracteur");
         StackPane root = new StackPane();
         root.getChildren().add(comboBox);
-        //root.getChildren().add(btn);
+        root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
+
